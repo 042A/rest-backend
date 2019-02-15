79072c3646af852bbc3c21667fd3dbfb6499b406
Johan Ahlstrand <042ahlstrand@gmail.com>
2019-02-15 22:35:05 +0100

    test2

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
-const express = require('express');
-var port = process.env.PORT || 3000;
-const app = express();
+var express = require('express');
+var app = express();
 
-app.listen(port, () => {
-    console.log('Server is running');
+app.get('/', function (req, res) {
+  res.send('Hello World!');
 });
+
+// use port 3000 unless there exists a preconfigured port
+var port = process.env.port || 3000;
+
+app.listen(port);
